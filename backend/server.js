3b0c77c642fdcaa2a3ab6631addc5ace5ea203bc
Mitch Sawaia <mjsawaia@gmail.com>
2022-06-22 15:02:29 -0400

    user auth with jwt

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleWare");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
